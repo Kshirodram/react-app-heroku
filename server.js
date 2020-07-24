150d1906e867b34c9767ebd7f488a84d443ea73f
Kshirodra Meher <kshirodra.meher@walmart.com>
2020-07-25 02:17:58 +0530

    added startup script

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const path = require("path");
 const compression = require("compression");
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 
 const app = express();
 app.use(compression());
