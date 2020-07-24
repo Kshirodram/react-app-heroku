f87b4325f51bb10cbf02cd0fda3a6b14b67df8d1
Kshirodra Meher <kshirodra.meher@walmart.com>
2020-07-25 02:08:31 +0530

    added startup script

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const port = process.env.port || 3000;
 const app = express();
 app.use(compression());
 
-app.use(express.static(path.join(__dirname, "/build")));
+app.use(express.static(path.join(__dirname, "/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/build", "index.html"));
+  res.sendFile(path.join(__dirname, "/dist", "index.html"));
 });
 
 http.createServer(app).listen(port, () => {
